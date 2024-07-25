c4efd0a6b4f7ee2099ec3c86ec8b6b2fc2c5b50a
matHeleno <matheuslopesheleno@gmail.com>
2024-07-24 21:31:49 -0300

    start signin increments

---
+++ src/hooks/auth.jsx
@@ -0,0 +1,44 @@
+import { createContext, useContext, useState } from "react";
+
+import { api } from "../services/api";
+
+export const AuthContext = createContext({})
+
+// eslint-disable-next-line react/prop-types
+function AuthProvider({ children }) {
+
+  const [data, setData] = useState({})
+
+  async function signIn({email, password}) {
+    
+    try {
+      const response = await api.post("/session", {email, password})
+      const {user, token} = response.data
+
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+      setData({user , token})
+    } catch(error) {
+      if (error.response) {
+        alert(error.response.data.message)
+      } else {
+        alert("Não foi possivel entrar.")
+      }
+    }
+  }
+
+  return (
+    // eslint-disable-next-line react/jsx-no-undef
+    <AuthContext.Provider value={{ signIn, user: data.user}}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+function useAuth() {
+  const context = useContext(AuthContext)
+
+  return context
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export { AuthProvider, useAuth }
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import { Background, Container, Form, InputContainer } from "./styles";
@@ -5,9 +6,21 @@ import { Background, Container, Form, InputContainer } from "./styles";
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
 
+import { useAuth } from "../../hooks/auth";
+
 
 export function SignIn() {
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+  
+  const { signIn } = useAuth()
+
+  function handleSignIn() {
+    signIn({ email, password})
+  }
+
   return(
+
     <Container>
       <Form>
         <h1>
@@ -17,10 +30,23 @@ export function SignIn() {
 
         <h2>Faça seu login</h2>
         <InputContainer>
-          <Input placeholder='E-mail'/>
-          <Input placeholder='Senha'/>
+          <Input 
+            placeholder='E-mail'
+            type="text"
+            onChange={e => setEmail(e.target.value)}
+          />
+
+          <Input 
+            placeholder='Senha'
+            type="password"
+            onChange={e => setPassword(e.target.value)}
+          />
         </InputContainer>
-        <Button title={'Entrar'}/>
+
+        <Button 
+          title={'Entrar'}
+          onClick={handleSignIn}
+        />
         <Link to={'/signup'}>
           Criar conta
         </Link>
